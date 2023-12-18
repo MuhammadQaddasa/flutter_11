73f65251079714275565a1e3b2cf011641d32249
2212010012 <muhammadqaddasa@gmail.com>
2023-12-18 21:43:54 +0700

    commit profile dan tugas

--- lib/Tugas.dart
+++ lib/Tugas.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class TugasWidget extends StatefulWidget {
   const TugasWidget({super.key, required this.title, required this.umur});
-final String title;
-final int umur; 
+  final String title;
+  final int umur;
 
   @override
   State<TugasWidget> createState() => _TugasWidgetState();
@@ -14,8 +14,8 @@ class _TugasWidgetState extends State<TugasWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-    Text(widget.title),
-    Text(widget.umur.toString()),
+        Text(widget.title),
+        Text(widget.umur.toString()),
       ],
     );
   }
